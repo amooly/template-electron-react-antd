fcf7259473436284bf3054637a5777c5ea1f3afb
墨黎 <haibin.whb@alibaba-inc.com>
2018-03-07 21:36:22 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var APP_PATH = path.resolve(__dirname, 'app');
+var SRC_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+    cache: true,
+    target: 'electron',
+
+    devtool: 'source-map',
+    entry: {
+        ui: './src/ui'
+    },
+    output: {
+        path: APP_PATH,
+        filename: '[name].js',
+        chunkFilename: '[chunkhash].js',
+        sourceMapFilename: '[name].map'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins: ['transform-class-properties']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        //new webpack.optimize.UglifyJsPlugin({comments: false}),
+        new webpack.EnvironmentPlugin(["NODE_ENV"]),
+        new CopyWebpackPlugin([
+            {from: path.resolve(SRC_PATH, 'main.js'), to: 'main.js'},
+            {from: path.resolve(SRC_PATH, 'backend'), to: 'backend'},
+            {from: path.resolve(SRC_PATH, 'ui/index.html'), to: 'index.html'},
+            {from: path.resolve(__dirname, 'package.json'), to: 'package.json'}
+        ])
+    ]
+};
